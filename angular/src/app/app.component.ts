3ddede39cb1591e4ee543b23cafc40a4cd4e6f3f
sophia anele <sophia.anele@redeye.com>
2019-11-15 10:03:21 +0000

    Extracted user data from facebook and populating the page

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './User';
+import { Fields } from './Fields';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,22 @@ import { User } from './User';
 })
 export class AppComponent implements OnInit {
   user: User;
+  fields: Fields;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     // Make the HTTP request:
     this.http.get<User>('http://localhost:8000/auth').subscribe(data => {
-      console.log(data);
       this.user = data;
+    });
+
+    // List of things i want to show: "first_name,last_name,picture,birthday,gender,posts";
+    var accessToken = 'EAAGbrjEyK6QBAOBdYbVtvdY5xqZCx7RiAifj4LhyslAxfdzAZBIkmC6wtg8OUJXbVbfQgpw6D7Sinpybkds61AnmX77EWpMzmbFIZBZA33ZCqhJsZB5QpjynvNPK7qmnHd7ZAnl5mj9z0Tl3dp3nCFOL3OrotWZA6ncbaNM9zZCN28kcNizZBKUc7r';
+
+    this.http.get('https://graph.facebook.com/v5.0/me?fields=picture,first_name%2Cbirthday,last_name,gender,posts&access_token=EAAGbrjEyK6QBAOBdYbVtvdY5xqZCx7RiAifj4LhyslAxfdzAZBIkmC6wtg8OUJXbVbfQgpw6D7Sinpybkds61AnmX77EWpMzmbFIZBZA33ZCqhJsZB5QpjynvNPK7qmnHd7ZAnl5mj9z0Tl3dp3nCFOL3OrotWZA6ncbaNM9zZCN28kcNizZBKUc7r').subscribe(result => {
+      console.log(result);
+      this.fields = result;
     });
 }
 }
